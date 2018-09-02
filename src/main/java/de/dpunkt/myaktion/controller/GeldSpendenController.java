51f940039260f458900402db0952abb5397cc3fa
Anna Ira <annairahurnaus@mytum.de>
2018-09-02 23:17:48 +0200

    5.6 ViewScope added

--- src/main/java/de/dpunkt/myaktion/controller/GeldSpendenController.java
+++ src/main/java/de/dpunkt/myaktion/controller/GeldSpendenController.java
@@ -1,17 +1,18 @@
 package de.dpunkt.myaktion.controller;
 
-import de.dpunkt.myaktion.model.Spende;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
-import javax.inject.Named;
 import java.io.Serializable;
 import java.util.logging.Logger;
 
-@SessionScoped
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import de.dpunkt.myaktion.model.Spende;
+
+@ViewScoped
 @Named
 public class GeldSpendenController implements Serializable {
 
