0c62d4efd2f27705b534e24ad324fbffd069f283
Anna Ira <annairahurnaus@mytum.de>
2018-09-02 13:27:13 +0200

    5.4 services

--- src/main/java/de/dpunkt/myaktion/data/AktionListProducer.java
+++ src/main/java/de/dpunkt/myaktion/data/AktionListProducer.java
@@ -1,19 +1,17 @@
 package de.dpunkt.myaktion.data;
 
 import java.io.Serializable;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import de.dpunkt.myaktion.model.Aktion;
-import de.dpunkt.myaktion.model.Konto;
-import de.dpunkt.myaktion.model.Spende;
-import de.dpunkt.myaktion.model.Spende.Status;
+import de.dpunkt.myaktion.services.AktionService;
 import de.dpunkt.myaktion.util.Events.Added;
 import de.dpunkt.myaktion.util.Events.Deleted;
 
@@ -21,6 +19,9 @@ import de.dpunkt.myaktion.util.Events.Deleted;
 public class AktionListProducer implements Serializable {
 
 	private static final long serialVersionUID = 3307596445605019126L;
+
+	@Inject
+	private AktionService aktionService;
 
 	private List<Aktion> aktionen;
 
@@ -32,58 +33,14 @@ public class AktionListProducer implements Serializable {
 
 	@PostConstruct
 	public void init() {
-		aktionen = createMockAktionen();
+		aktionen = aktionService.getAllAktionen();
 	}
 
 	public void onAktionAdded(@Observes @Added Aktion aktion) {
 		getAktionen().add(aktion);
 	}
-	
+
 	public void onAktionDeleted(@Observes @Deleted Aktion aktion) {
 		getAktionen().remove(aktion);
-	}
-
-	private List<Aktion> createMockAktionen() {
-		Spende spende1 = new Spende();
-		spende1.setSpenderName("Heinz Schmidt");
-		spende1.setBetrag(20d);
-		spende1.setQuittung(true);
-		spende1.setStatus(Status.UEBERWIESEN);
-		spende1.setKonto(new Konto(spende1.getSpenderName(), "XXX Bank", "123456", "87654321"));
-
-		Spende spende2 = new Spende();
-		spende2.setSpenderName("Karl Meier");
-		spende2.setBetrag(30d);
-		spende2.setQuittung(false);
-		spende2.setStatus(Status.IN_BERARBEITUNG);
-		spende2.setKonto(new Konto(spende2.getSpenderName(), "YYY Bank", "654321", "86427531"));
-
-		List<Spende> spenden = new LinkedList<>();
-		spenden.add(spende1);
-		spenden.add(spende2);
-
-		Aktion aktion1 = new Aktion();
-		aktion1.setName("Trikots für A-Jugend");
-		aktion1.setSpendenZiel(1000d);
-		aktion1.setBisherGespendet(258d);
-		aktion1.setSpendenBetrag(20d);
-		aktion1.setId(1L);
-		aktion1.setKonto(new Konto("Max Mustermann", "ABC Bank", "100200300", "12345678"));
-		aktion1.setSpenden(spenden);
-
-		Aktion aktion2 = new Aktion();
-		aktion2.setName("Rollstuhl für Maria");
-		aktion2.setSpendenZiel(2500d);
-		aktion2.setBisherGespendet(742d);
-		aktion2.setSpendenBetrag(25d);
-		aktion2.setId(2L);
-		aktion2.setKonto(aktion1.getKonto());
-		aktion2.setSpenden(spenden);
-
-		List<Aktion> ret = new LinkedList<>();
-		ret.add(aktion1);
-		ret.add(aktion2);
-
-		return ret;
 	}
 }
---
+++ src/main/java/de/dpunkt/myaktion/services/AktionService.java
@@ -0,0 +1,11 @@
+package de.dpunkt.myaktion.services;
+
+import java.util.List;
+
+import de.dpunkt.myaktion.model.Aktion;
+
+public interface AktionService {
+
+	List<Aktion> getAllAktionen();
+
+}
